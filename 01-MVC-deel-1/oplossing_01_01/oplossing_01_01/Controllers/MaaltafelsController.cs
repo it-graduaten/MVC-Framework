76b2395345d6e1574d808ccc607ffd7eacace8d5
QuirkyDev <joeri.verlooy@gmail.com>
2023-06-07 16:06:48 +0200

    oplossing_01_01: Added basic controller and views

---
+++ 01-MVC-deel-1/oplossing_01_01/oplossing_01_01/Controllers/MaaltafelsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace oplossing_01_01.Controllers
+{
+    public class MaaltafelsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
